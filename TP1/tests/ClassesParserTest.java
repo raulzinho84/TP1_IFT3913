9ca77c1e3f68b6a6f5920ae49089f120141d1d4a
Vincent Moore <moore505@live.ca>
2020-10-04 21:18:23 -0400

    Setting up tests environment

---
+++ TP1/tests/ClassesParserTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClassesParserTest {
+
+    @Test
+    public void classe_NCLOC() {
+        System.out.println("Test exécuté");
+    }
+
+    @Test
+    public void testClasse_NCLOC() {
+    }
+
+    @Test
+    public void classe_LOC() {
+    }
+
+    @Test
+    public void classe_CLOC() {
+    }
+
+    @Test
+    public void classe_DC() {
+    }
+}
---
+++ TP1/tests/MethodesParserTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MethodesParserTest {
+
+    @Test
+    public void calculerPredicats() {
+    }
+
+    @Test
+    public void methode_LOC() {
+    }
+
+    @Test
+    public void methode_CLOC() {
+    }
+
+    @Test
+    public void methode_NCLOC() {
+    }
+
+    @Test
+    public void methode_DC() {
+    }
+}
