72a3fd2635589d97b203591628100507b2175426
raulzinho84 <wael.abou.ali@umontreal.ca>
2020-10-07 05:25:54 -0400

    Update MethodesParser.java

--- TP1/src/MethodesParser.java
+++ TP1/src/MethodesParser.java
@@ -6,44 +6,40 @@ import java.util.List;
 public class MethodesParser extends Parser {
 
     private static boolean isSwitch;
-    private List<List<String>> complexityFile;
 
     /**
-     *
-     * @param firstRow
+     *  Constructeur de la classe MethodesParser.
+     * @param firstRow C'est les valeurs de la premiere ligne pour MethodesParser.
      */
     public MethodesParser(List<String> firstRow) {
         super(firstRow, Arrays.asList("chemin",
                 "class", "methode", "methode_CLOC", "methode_LOC", "methode_DC",
                 "methode_BC"));
-        this.complexityFile = new ArrayList<>();
         this.addcomplexityArray(Arrays.asList("CC", "methode_BC"));
     }
 
     /**
-     *
-     * @return
+     * Une methode qui retourne la valeur retouornee si la phrase contient
+     * un switch ou non.
+     * @return La valeur de la presence d'un switch dans une phrase ou non.
      */
     public boolean isSwitch() { return isSwitch; }
 
     /**
-     *
-     * @param aSwitch
+     * Setter.
+     * @param aSwitch Changer la valeur booleene de isSwitch a une nouvelle.
      */
     public void setSwitch(boolean aSwitch) { isSwitch = aSwitch; }
 
-    @Override
-    List<String> treatCode(String currentClass) {
-        return super.treatCode(currentClass);
-    }
-
     /**
-     *
-     * @param chemin
-     * @param className
-     * @return
+     * Methode pour extraire les methodes contenues dans un site web qui
+     * represente un fichier java.
+     * @param chemin Le raccourci du site web qui contient le code qu'on va
+     *               extraire les methodes a partir de lequel.
+     * @param className Le nom de la classe qui contient les methodes a extraire.
+     * @return Le total de la complexite WMC d'une classe.
      */
-    int getFileMethodes(String chemin, String className, float totalClasseDC) {
+    int getFileMethodes(String chemin, String className) {
         String firstMethodeName = null, secondMethodeName, methodeName, tempMethodeName;
         int lineCodeIndex = 0, numPredicats = 0, totalPredicats = 0;
         List<String> capturedMethode = new ArrayList<>(), reparsedMethode,
@@ -95,10 +91,10 @@ public class MethodesParser extends Parser {
     }
 
     /**
-     *
-     * @param element
-     * @param lineElements
-     * @return
+     * Methode pour extraire le nom de la methode cherchee.
+     * @param element La methode au complet avec les commentaires.
+     * @param lineElements La premiere ligne sous forme des mots dans un tableau.
+     * @return Le nom de la methode sous forme de nom(xxx)_1er param(xxx)_ ... .
      */
     String getMethodName(String element, String[] lineElements) {
         String prevItem = null, prevPrevItem = null;
@@ -127,7 +123,8 @@ public class MethodesParser extends Parser {
                 } else {
                     if(itemIndex >=2) {
                         if(lineElements.length - 1 >= itemIndex + 1) {
-                            if ((prevItem.contentEquals("final") && prevPrevItem.contentEquals("static")) ||
+                            if ((prevItem.contentEquals("final") &&
+                                    prevPrevItem.contentEquals("static")) ||
                                     prevItem.contentEquals("static")) {
                                 if(lineElements[itemIndex + 1].contains("(")) {
                                     tempString.append(lineElements[itemIndex + 1]);
@@ -143,9 +140,10 @@ public class MethodesParser extends Parser {
     }
 
     /**
-     *
-     * @param lineCodeIndex
-     * @return
+     * Methoude pour ajouter les commentaires de JAVADOC qui se trouve avant
+     * la methode a exploiter.
+     * @param lineCodeIndex C'est l'index de la ligne de code courante.
+     * @return tous les commentaires JAVADOC trouvees avant une methode.
      */
     List<String> addJavaDOC(int lineCodeIndex) {
         int prevLineIndex = lineCodeIndex - 1;
@@ -163,9 +161,12 @@ public class MethodesParser extends Parser {
     }
 
     /**
-     *
-     * @param capturedMethode
-     * @return
+     * Methode pour extraire le corps de la methode a exploiter qui commence
+     * avec un "{" et se termine avec "}".
+     * @param capturedMethode La liste des lignes du code qui commence a partir
+     *                        de la premiere ligne de la methode et se termine
+     *                        dans la boucle si on trouve la "}" fermante.
+     * @return Une liste contenant les lignes du code d'une methode.
      */
     List<String> reparseMethode(List<String> capturedMethode) {
         int count = 0;
@@ -185,11 +186,12 @@ public class MethodesParser extends Parser {
     }
 
     /**
-     *
-     * @param splittedLineCode
-     * @param comparator
-     * @param comparatorRegex
-     * @return
+     * Methode pour iterer a travers les lignes du code pour trouver les "(" ou ")" ou "()".
+     * @param splittedLineCode Le code a exploiter.
+     * @param comparator Dans ce cas ca sera seulement "()".
+     * @param comparatorRegex Ca sera ("//(//)") qui represente la representation
+     *                        REGEX pour les "(" ouvrante et ")" fermante.
+     * @return le nom de la methode extrait sans "(", ")" ou "()".
      */
     String iterateAndSplitArray(String[] splittedLineCode,
                                               String comparator,
@@ -200,9 +202,10 @@ public class MethodesParser extends Parser {
     }
 
     /**
-     *
-     * @param splittedLineCode
-     * @return
+     * Methode pour creer le nom de la methode si tous les params sont sur la
+     * meme ligne.
+     * @param splittedLineCode Liste des mots qui se trouvent dans une phrase.
+     * @return Un String sous forme xxx_xxx_... .
      */
     String buildStringFromListOneLine(String[] splittedLineCode) {
         StringBuilder extractedMethode = new StringBuilder();
@@ -215,8 +218,7 @@ public class MethodesParser extends Parser {
                 if(element.contains(",")) {
                     splitElements = element.split("(?!^)");
                     if(splitElements[0].contentEquals(",")) {
-                        extractedMethode.append("_").append(removeFirstChar(element,
-                                splitElements));
+                        extractedMethode.append("_").append(removeFirstChar(splitElements));
                     } else {
                         if(splitElements[splitElements.length - 1].contentEquals(",") &&
                                 (currIndex != splittedLineCode.length - 1)) {
@@ -231,10 +233,11 @@ public class MethodesParser extends Parser {
     }
 
     /**
-     *
-     * @param reparsedMethode
-     * @param lineCode
-     * @return
+     * Methode qui va iterer a travers les lignes pour extraire tous les params
+     * contenus dans une methode.
+     * @param reparsedMethode Le code de la methode a traiter.
+     * @param lineCode La ligne du code actuelle.
+     * @return Un String sous forme xxx_xxx_... .
      */
     String buildStringFromMultipleLines(List<String> reparsedMethode,
                                                       String lineCode) {
@@ -252,9 +255,9 @@ public class MethodesParser extends Parser {
     }
 
     /**
-     *
-     * @param element
-     * @return
+     * Methodes pour enlever toutes les espaces blancs.
+     * @param element La ligne du code qui represente la phrase a traiter.
+     * @return Une liste contenant les mots de la phrase.
      */
     String[] getLineElements(String element) {
         element = element.replaceAll("\\s+", "+");
@@ -263,13 +266,14 @@ public class MethodesParser extends Parser {
     }
 
     /**
-     *
-     * @param reparsedMethode
-     * @param chemin
-     * @param currClasse
-     * @param methodeName
-     * @param numPredicats
-     * @return
+     * Methode qui ajoute les donnees obtenues par les calculs dans les
+     * listes(attributs) de la classe.
+     * @param reparsedMethode La liste contenant les lignes du code a evaluer.
+     * @param chemin Le lien de la page web courante.
+     * @param currClasse La classe courante qui contient la methode extraite.
+     * @param methodeName Le nom de la methode extraite.
+     * @param numPredicats Le numero total des predicats calculees.
+     * @return Methode_BC pour la ajouter dans la liste(DC,CC).
      */
     float addMethodeData(List<String> reparsedMethode,
                                 String chemin, String currClasse,
@@ -299,10 +303,11 @@ public class MethodesParser extends Parser {
 
 
     /**
-     *
-     * @param reparsedMethode
-     * @param firstmethodeName
-     * @return
+     * Methode pour extraire le nom de la methode soit sur une ou plusieurs lignes.
+     * @param reparsedMethode La liste des lignes du code a evaluer.
+     * @param firstmethodeName Le nom de la premiere methode pour la ajouter au
+     *                         String dont le format est xxx_xxx_... .
+     * @return Le nom de la methode extraite et nulle si aucune methode est trouvee.
      */
     String extractMethodeName(List<String> reparsedMethode,
                                             String firstmethodeName) {
@@ -327,13 +332,15 @@ public class MethodesParser extends Parser {
     }
 
     /**
-     *
-     * @param reparsedMethode
-     * @return
+     * Methode pour calculer le nombre total des predicats soit if, else if,
+     * while ou switch. Cette methode va s'assurer que les lignes ne sont pas
+     * des commentaires et ce sont des lignes du code.
+     * @param reparsedMethode La liste des lignes du code a evaluer.
+     * @return Le numero total des predicats.
      */
     int calculerPredicats(List<String> reparsedMethode) {
         List<String> elementLine;
-        int totalPredicats = 0, temp;
+        int totalPredicats = 0;
         for(String lineCode : reparsedMethode) {
             elementLine = separateAndRemoveSpaces(lineCode);
             if(!elementLine.get(0).contains("/*") &&
@@ -346,9 +353,10 @@ public class MethodesParser extends Parser {
     }
 
     /**
-     *
-     * @param lineCode
-     * @return
+     * Methode qui calculer les if, else if, while et switch.
+     * @param lineCode La ligne du code a evaluer.
+     * @return  Le numero total des predicats et 0 s'il n'existe pas des
+     *          branchements dans le code.
      */
     int handlePredicats(String lineCode) {
         if(lineCode.contains("if(") || lineCode.contains("if (")  ||
@@ -409,10 +417,10 @@ public class MethodesParser extends Parser {
     }
 
     /**
-     *
-     * @param methode_DC
-     * @param CC
-     * @return
+     * Methode pour calculer la complexite d'une methode.
+     * @param methode_DC DC
+     * @param CC CC
+     * @return DC/CC
      */
     float methode_BC(float methode_DC, float CC) {
         return calculer_BC(methode_DC, CC);
